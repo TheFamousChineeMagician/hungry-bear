6446b639075f6ba5fa214d78ea84a5a11570f9d8
TheFamousChineeMagician <mli007@hawaii.rr.com>
2017-06-20 14:59:08 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function eatHumans(bearName, humanNames){
+	console.log(bearName);
+	console.log(humanNames);
+	console.log("Kazu ate " + humanNames[2])
+}
+
+eatHumans("Kazu", ["Matt", "Gavin", "Krystyn"]); //length 3
